71b04df32c946b7055c5b89b0e1726daacb42099
Austin Schrader <austin.jschrader@gmail.com>
2022-04-25 00:17:32 -0700

    Initialize orm with post

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+
+  const post = orm.em.create(Post, { title: "My first post!" });
+  await orm.em.persistAndFlush(post);
+};
 console.log("hello world");
 
-const orm = MikroORM.init();
-
+main().catch((err) => {
+  console.error(err);
+});
